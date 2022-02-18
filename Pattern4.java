b1a5936a51701f06899a8e4068723c5e48cb3b4c
Aryan Raj <aryanraj2713@gmail.com>
2022-02-18 19:48:21 +0530

    p4

---
+++ Pattern4.java
@@ -0,0 +1,23 @@
+
+
+class Pattern4 {
+    public static void main(String[] args) {
+        
+        int n = 10;
+        int i=1;
+        while(i<=n){
+            int j=1;
+            while(j<=i){
+                System.out.print("*");
+                j++;
+            }
+            i++;
+            System.out.println();
+        }
+    }
+}
+
+
+    
+
+
